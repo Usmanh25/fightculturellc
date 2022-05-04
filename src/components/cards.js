71598e72773894c0d7c2999451d50b2bcb9488a8
usmanh25 <usmanh25@yahoo.com>
2022-05-04 01:16:39 -0700

    updating homepage components

--- src/components/cards.js
+++ src/components/cards.js
@@ -10,7 +10,38 @@ function Cards() {
             <div className='cards__container'>
                 <div className='cards_wrapper'>
                     <ul className='cards__item'>
-                        <CardItem />
+                        <CardItem 
+                        src='images/img-9.jpg'
+                        text='ample text helloooo'
+                        label='labeltagggg'
+                        path='/classes'
+                        />
+                        <CardItem 
+                        src='images/img-9.jpg'
+                        text='ample text helloooo 22222'
+                        label='labeltagggg222'
+                        path='/classes'
+                        />
+                    </ul>
+                    <ul className='cards__item'>
+                        <CardItem 
+                        src='images/img-9.jpg'
+                        text='ample text helloooo'
+                        label='labeltagggg'
+                        path='/classes'
+                        />
+                        <CardItem 
+                        src='images/img-9.jpg'
+                        text='ample text helloooo 22222'
+                        label='labeltagggg222'
+                        path='/classes'
+                        />
+                        <CardItem 
+                        src='images/img-9.jpg'
+                        text='ample text helloooo 22222'
+                        label='labeltagggg222'
+                        path='/classes'
+                        />
                     </ul>
                 </div>
             </div>
