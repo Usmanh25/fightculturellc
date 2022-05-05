2efe62269f73248fc3866c03c458edbe818f5275
usmanh25 <usmanh25@yahoo.com>
2022-05-05 14:34:40 -0700

    making footer

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="footer-container">
+            <section className="footer-subscription">
+                <p className="footer-subscription-heading">
+                    Join the culture!
+                </p>
+                <p className="footer-subscription-text">
+                    Sign up today 
+                </p>
+            </section>
+        </div>
+    )
+}
+export default Footer
