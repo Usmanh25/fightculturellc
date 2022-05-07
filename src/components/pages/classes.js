28cd9e5a8b2b301fec2d693a7f5e8295c043717d
usmanh25 <usmanh25@yahoo.com>
2022-05-06 18:29:33 -0700

    editing classes

--- src/components/pages/classes.js
+++ src/components/pages/classes.js
@@ -1,19 +1,84 @@
 import React from 'react';
 import '../../App.css';
+import './classes.css';
+
 
 function Classes() {
     return (
         <>
-            <div className='classes'>
+            <div className='classes-div'>
                 <h1> Our Services </h1>
 
-                <div>
-                    <h1> Muay Thai </h1>
-                    <img src='images/Sample1.png'></img>
-                    <p>lorem ipsum</p>
+                <div className='class-div'>
+                    <div className='class-image'>
+                        <img src='images/Sample1.png'></img>
+                    </div>
+                    <div className='class-text'>
+                        <h1> Muay Thai </h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            esse cillum dolore eu fugiat nulla pariatur. 
+                            Excepteur sint occaecat cupidatat non proident</p>
+                    </div>
+                </div>
+                <div className='class-div'>
+                    <div className='class-image'>
+                        <img src='images/Sample1.png'></img>
+                    </div>
+                    <div className='class-text'>
+                        <h1> Muay Thai </h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            esse cillum dolore eu fugiat nulla pariatur. 
+                            Excepteur sint occaecat cupidatat non proident</p>
+                    </div>
+                </div>
+                <div className='class-div'>
+                    <div className='class-image'>
+                        <img src='images/Sample1.png'></img>
+                    </div>
+                    <div className='class-text'>
+                        <h1> Muay Thai </h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            esse cillum dolore eu fugiat nulla pariatur. 
+                            Excepteur sint occaecat cupidatat non proident</p>
+                    </div>
+                </div>
+                <div className='class-div'>
+                    <div className='class-image'>
+                        <img src='images/Sample1.png'></img>
+                    </div>
+                    <div className='class-text'>
+                        <h1> Muay Thai </h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            esse cillum dolore eu fugiat nulla pariatur. 
+                            Excepteur sint occaecat cupidatat non proident</p>
+                    </div>
+                </div>
+                <div className='class-div'>
+                    <div className='class-image'>
+                        <img src='images/Sample1.png'></img>
+                    </div>
+                    <div className='class-text'>
+                        <h1> Muay Thai </h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            esse cillum dolore eu fugiat nulla pariatur. 
+                            Excepteur sint occaecat cupidatat non proident</p>
+                    </div>
                 </div>
 
-                <h1> Boxing </h1>
+                <div className='class-iv'>
+                    <h1> Boxing </h1>
+                    <img src='images/Sample1.png'></img>
+                    <p>Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit, sed do eiusmod tempor 
+                        esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident</p>
+                </div>
+
                 <h1> Jiu-Jitsu </h1>
                 <h1> Wrestling </h1>
             </div>
