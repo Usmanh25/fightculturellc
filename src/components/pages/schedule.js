776265f5cdf7231cc004fac60329ce62279cf832
usmanh25 <usmanh25@yahoo.com>
2022-05-06 17:25:10 -0700

    editing schedule

--- src/components/pages/schedule.js
+++ src/components/pages/schedule.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import '../../App.css';
+import './schedule.css';
 
-export default function Schedule() {
-    return <h1 className='schedule'>Weekly Class Schedule</h1>
+
+function Schedule() {
+    return(
+        <>
+            <div>
+                <h1 className='schedule'>Weekly Class Schedule</h1>
+            </div>
+
+            <div className='full-schedule'>
+
+                <div className='monday'>
+                    <h3>Monday</h3>
+                </div>
+
+                <div className='tuesday'>
+                    <h3>Monday</h3>
+                </div>
+
+                <div className='wednesday'>
+                    <h3>Monday</h3>
+                </div>
+
+                <div className='thursday'>
+                    <h3>Monday</h3>
+                </div>
+
+                <div className='friday'>
+                    <h3>Monday</h3>
+                </div>
+                
+            </div>
+
+        </>
+    )
 }
+
+export default Schedule;
