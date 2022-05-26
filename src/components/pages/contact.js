1aa0b05bd7d9f053653222c97fcc7f9f9b524ea5
usmanh25 <usmanh25@yahoo.com>
2022-05-26 01:41:33 -0700

    css

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -17,7 +17,7 @@ function Contact() {
                     </div>
 
                     <div className='contact-info'>
-                        <h1>(415)5555</h1>
+                        <h1>(415)123-4567</h1>
                         <h1>fightculture@email.com</h1>
                         <h1>6137 Mission Street Daly City, CA 94014</h1>
                     </div>
