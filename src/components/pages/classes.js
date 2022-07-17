f58562aab8e121e177f07ca4b73342d47e4129c9
usmanh25 <usmanh25@yahoo.com>
2022-07-17 15:51:26 -0700

    editing instructors page

--- src/components/pages/classes.js
+++ src/components/pages/classes.js
@@ -12,7 +12,7 @@ function Classes() {
 
             <div className='classes-div'>
 
-            <h1 className='classes-title'>Our Clses</h1>
+            <h1 className='classes-title'>Martial Arts Classes</h1>
 
                 <div className='class-div'>
                     <div className='class-image'>
@@ -20,14 +20,14 @@ function Classes() {
                     </div>
                     <div className='class-text'>
                     <h1> Muay Thai </h1>
-                        <p>60 minue high-intensity class focusing on 
-                            Muay Thai foowork, striking, & padholding</p>
-                            <br/>
+                        <p>60 minute session covering aspects of Muay Thai 
+                            from foowork, padwork, striking, and sparring</p>
+                            {/* <br/>
                         <li> 15 min Wamup Dynamic Stretching / Shadowboxing </li>
                         <li> 20 min Cobiains </li>
                         <li> 20 minLiv Drlling </li>
                         <li> 10 min io / Cool Off </li>
-                        <li> 10 min Cadio /Col Off </li>
+                        <li> 10 min Cadio /Col Off </li> */}
                     </div>
                 </div>
 
@@ -37,13 +37,13 @@ function Classes() {
                     </div>
                     <div className='class-text'>
                     <h1> Boxing </h1>
-                        <p>60 minute high-intensity class focusing on 
-                            Boxing footwork, strikes, & padwork</p>
-                        <br/>
+                        <p>60 minute session covering aspects of Boxing 
+                            from foowork, padwork, striking, and sparring</p>
+                        {/* <br/>
                         <li> 10 min Dynamic Stretching / Shadowboxing </li>
                         <li> 20 min Combinations </li>
                         <li> 20 min Live Drilling </li>
-                        <li> 10 min Cardio / Cool Off </li>
+                        <li> 10 min Cardio / Cool Off </li> */}
                     </div>
                 </div>
 
@@ -53,16 +53,16 @@ function Classes() {
                     </div>
                     <div className='class-text'>
                         <h1> Jiu Jitsu </h1>
-                        <p>60 minute fundamentals class focusing on 
-                            basic moves, positions, and concepts</p>
-                            <br/>
+                        <p>90 minute class focusing on basic Brazilian 
+                            Jiu-Jitsu moves, positioning, concepts, and live rolling</p>
+                            {/* <br/>
                         <p>60 minute situational class focusing on 
                             drilling moves, advanced concepts, and live rolling</p>
                             <br/>
                         <li> 10 min Stretching </li>
                         <li> 20 min Drilling </li>
                         <li> 20 min Live Drilling </li>
-                        <li> 10 min Cardio / Cool Off </li>
+                        <li> 10 min Cardio / Cool Off </li> */}
                     </div>
                 </div>
 
@@ -72,13 +72,13 @@ function Classes() {
                     </div>
                     <div className='class-text'>
                     <h1> Wrestling </h1>
-                        <p>60 minte class focusing on 
-                            positions, moves, & concepts</p>
-                            <br/>
+                        <p>60 minute class focusing on Wresling stretching, 
+                            moves, positioning, concepts, and live action</p>
+                            {/* <br/>
                         <li> 10 min Stretching </li>
                         <li> 20 min Driling </li>
                         <li> 20 min Livrilling </li>
-                        <li> 10 min Cardio / Cool Off </li>
+                        <li> 10 min Cardio / Cool Off </li> */}
                     </div>
                 </div>
                 
