04ee39065528db686eb55851dcff96da2debb306
usmanh25 <usmanh25@yahoo.com>
2022-08-14 16:46:22 -0700

    classes edited

--- src/components/pages/classes.js
+++ src/components/pages/classes.js
@@ -20,14 +20,13 @@ function Classes() {
                     </div>
                     <div className='class-text'>
                         <h1> Muay Thai </h1>
-                            <p>60 minute session covering aspects of Muay Thai 
-                                from foowork, padwork, striking, and sparring</p>
-                                {/* <br/>
-                            <li> 15 min Wamup Dynamic Stretching / Shadowboxing </li>
-                            <li> 20 min Cobiains </li>
-                            <li> 20 minLiv Drlling </li>
-                            <li> 10 min io / Cool Off </li>
-                            <li> 10 min Cadio /Col Off </li> */}
+                            <p>
+                                Muay thai, or Thai boxing, is a style of fighting which utilizes "eight limbs": fists, 
+                                legs, elbows, and knees. A centuries old practice, we teach authentic Muay Thai and cover basic 
+                                and advanced techniques. At Fight Culture, our 60 minute session covers all aspects of Muay Thai 
+                                training including footwork, pad holding, striking, defense, and sparring. This is a moderate-to-high 
+                                intensity workout.
+                            </p>
                     </div>
                 </div>
 
@@ -37,13 +36,12 @@ function Classes() {
                     </div>
                     <div className='class-text'>
                     <h1> Boxing </h1>
-                        <p>60 minute session covering aspects of Boxing 
-                            from foowork, padwork, striking, and sparring</p>
-                        {/* <br/>
-                        <li> 10 min Dynamic Stretching / Shadowboxing </li>
-                        <li> 20 min Combinations </li>
-                        <li> 20 min Live Drilling </li>
-                        <li> 10 min Cardio / Cool Off </li> */}
+                        <p>
+                            For fans of the sweet science, our newly added boxing class will provide you one of 
+                            the best cardio-vascular workouts you've had! This session is 60 minutes long and 
+                            will instill boxing fundamentals in your movement, defense, and striking. Much like 
+                            Muay Thai, this class is a great for conditioning, stress-relief, and self-defense! 
+                        </p>
                     </div>
                 </div>
 
@@ -52,17 +50,14 @@ function Classes() {
                         <img src='images/JiuJitsu.png' alt='jiujitsupic'></img>
                     </div>
                     <div className='class-text'>
-                        <h1> Jiu Jitsu </h1>
-                        <p>90 minute class focusing on basic Brazilian 
-                            Jiu-Jitsu moves, positioning, concepts, and live rolling</p>
-                            {/* <br/>
-                        <p>60 minute situational class focusing on 
-                            drilling moves, advanced concepts, and live rolling</p>
-                            <br/>
-                        <li> 10 min Stretching </li>
-                        <li> 20 min Drilling </li>
-                        <li> 20 min Live Drilling </li>
-                        <li> 10 min Cardio / Cool Off </li> */}
+                        <h1> Brazilian Jiu Jitsu </h1>
+                        <p>
+                            Brazilian Jiu-Jitsu, developed by the Gracie family during the early twentieth 
+                            century, is a close-combat martial arts system which focuses on ground fighting 
+                            and submission holding. Our 90 minute Brazilian Jiu-Jitsu classes begin with group stretches, 
+                            followed by technique drilling, new concept & positioning instruction, and then live rolling. 
+                        </p>
+
                     </div>
                 </div>
 
@@ -72,13 +67,12 @@ function Classes() {
                     </div>
                     <div className='class-text'>
                     <h1> Wrestling </h1>
-                        <p>60 minute class focusing on Wresling stretching, 
-                            moves, positioning, concepts, and live action</p>
-                            {/* <br/>
-                        <li> 10 min Stretching </li>
-                        <li> 20 min Driling </li>
-                        <li> 20 min Livrilling </li>
-                        <li> 10 min Cardio / Cool Off </li> */}
+                        <p>
+                            We offer a 60 minute Wrestling session on Fridays for those looking to sharpen their grappling skills. 
+                            Much like Brazilian Jiu-Jitsu, this moderate-to-high intensity class begins with dynamic stretching,
+                            follows up with technique teaching and drilling, and then ends with live wrestling. 
+                        </p>
+
                     </div>
                 </div>
                 
