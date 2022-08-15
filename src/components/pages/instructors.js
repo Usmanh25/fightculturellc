a8834096a5d47af301c28122fb6c02207681f72c
usmanh25 <usmanh25@yahoo.com>
2022-08-14 18:55:23 -0700

    css

--- src/components/pages/instructors.js
+++ src/components/pages/instructors.js
@@ -21,11 +21,11 @@ function Instructors() {
                     <div className='instructor-text'>
                         <h1> Kyle Riddeau </h1>
                         <h2> Jiu-Jitsu </h2>
-                        <h3> Monday </h3>
-                        <h3> Wednesday </h3>
-                        <h3> Thursday </h3>
+                        <h3> Monday 6:30pm </h3>
+                        <h3> Wednesday 6:30pm</h3>
+                        <h3> Thursday 6:30pm</h3>
                         <h2> Muay Thai </h2>
-                        <h3> Thursday </h3>
+                        <h3> Thursday 5pm</h3>
                         <p>  
                             Kyle is a ___th degree black belt under 
                             ______ who has professionally 
@@ -43,8 +43,8 @@ function Instructors() {
                     <div className='instructor-text'>
                     <h1> Jeremy Jacildo</h1>
                     <h2> Muay Thai </h2>
-                    <h3> Monday </h3>
-                    <h3> Tuesday </h3>
+                    <h3> Monday 5pm</h3>
+                    <h3> Tuesday 5pm</h3>
                         <p>     
                             Jeremy is our Muay Thai specialist. A former professional,
                             he has been training muay thai since 2006. He likes long 
@@ -60,7 +60,7 @@ function Instructors() {
                     <div className='instructor-text'>
                         <h1> Martin Ruiz </h1>
                         <h2> Wrestling </h2>
-                        <h3> Friday </h3>
+                        <h3> Friday 6:30pm</h3>
                         <p>
                             Martin is a former NCAA
                             wrestler with _____ matches of experience who has been 
@@ -76,10 +76,10 @@ function Instructors() {
                     <div className='instructor-text'>
                         <h1> Ritchie Mercado </h1>
                         <h2> Muay Thai </h2>
-                        <h3> Monday </h3>
-                        <h3> Tuesday </h3>
-                        <h3> Wednesday </h3>
-                        <h3> Thursday </h3>
+                        <h3> Monday 8am</h3>
+                        <h3> Tuesday 9am</h3>
+                        <h3> Wednesday 8am</h3>
+                        <h3> Thursday 9am</h3>
                         <p>
                             Ritchie is our morning instructor. He has been training and competing Muay Thai for _____ years.
                             A versatile and skilled martial artist, he also has a brown belt in Brazilian Jiu-Jitsu. 
@@ -95,7 +95,7 @@ function Instructors() {
                     <div className='instructor-text'>
                         <h1> Ron Nitafan </h1>
                         <h2> Muay Thai </h2>
-                        <h3> Wednesday </h3>
+                        <h3> Wednesday 5pm</h3>
                         <p>  
                             Ron is one of our beloved Muay Thai trainers with over 10 years
                             of training, competing, and instructing under his belt. A retired 
@@ -111,7 +111,7 @@ function Instructors() {
                     <div className='instructor-text'>
                         <h1> Ryan Wellman </h1>
                         <h2> Jiu-Jitsu </h2>
-                        <h3> Tuesday </h3>
+                        <h3> Tuesday 6:30pm</h3>
                         <p>  
                             Ryan is a Brazilian Jiu-Jitsu brown belt under
                             ____ who has _____ matches of experience and 
@@ -127,8 +127,8 @@ function Instructors() {
                     <div className='instructor-text'>
                         <h1> Brandy Cotter </h1>
                         <h2> Boxing </h2>
-                        <h3> Friday </h3>
-                        <h3> Saturday </h3>
+                        <h3> Friday 8am</h3>
+                        <h3> Saturday 9am</h3>
                         <p>Brandy has been training boxing for 
                             _____ years and (more info about Brandy here) (Fun fact)
                         </p>
