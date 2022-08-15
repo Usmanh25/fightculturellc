bb4a14ac493e1b880be2b7698ff938e65e80ab1c
usmanh25 <usmanh25@yahoo.com>
2022-08-15 02:49:30 -0700

    cs

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -7,8 +7,6 @@ function HeroSection() {
         <div className='hero-container'>
             
             <video src='/videos/f_c_video.mp4' autoPlay loop muted id="myVideo"/>
-            {/* <h1>Fight Culture SF</h1>
-            <p> 6137 Mission Street</p> */}
 
         </div>
     )
